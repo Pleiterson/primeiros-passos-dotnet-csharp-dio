08a7572483c76d45c1dbf8419db89d0c0fb8a022
pleiterson <pleiterson@gmail.com>
2021-05-27 17:39:52 -0300

    first commit e conclusao da aula iniciando dotnet

---
+++ iniciando-dotnet/Program.cs
@@ -0,0 +1,13 @@
+﻿using System; // pacote nativo do dotnet que nos fornece as ferramentas utilizáveis durante o desenvolvimento do projeto (ex.: Console.WriteLine("");)
+
+namespace iniciando_dotnet {
+  class Program {
+    static void Main(string[] args) {
+      int numVezes = 5;
+
+      for (int i = 0; i < numVezes; i++) {
+        Console.WriteLine($"Bem-vindo ao curso de .NET {i}");
+      }
+    }
+  }
+}
