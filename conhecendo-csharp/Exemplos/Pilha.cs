1bce27f944a5962a3e9e3139b738a98e187ad6ab
pleiterson <pleiterson@gmail.com>
2021-05-28 16:49:41 -0300

    concluida a aula Estrutura de programa

---
+++ conhecendo-csharp/Exemplos/Pilha.cs
@@ -0,0 +1,31 @@
+using System;
+
+namespace conhecendo_csharp.Exemplos { // sempre seguir o namespace com a pasta, uma boa prática
+  public class Pilha {
+    Posicao primeiro;
+
+    public void Empilha(object item) {
+      primeiro = new Posicao(primeiro, item);
+    }
+
+    public object Desempilha() {
+      if (primeiro == null) {
+        throw new InvalidOperationException("A pilha está vazia!");
+      }
+
+      object resultado = primeiro.item;
+      primeiro = primeiro.proximo;
+
+      return resultado;
+    }
+    
+    class Posicao {
+      public Posicao proximo;
+      public object item;
+      public Posicao(Posicao proximo, object item) {
+        this.proximo = proximo;
+        this.item = item;
+      }
+    }
+  }
+}
---
+++ conhecendo-csharp/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using conhecendo_csharp.Exemplos;
+
+namespace conhecendo_csharp {
+  class Program {
+    static void Main(string[] args) {
+      var s = new Pilha(); // ou var s = new Exemplos.Pilha(); assim não teria a necessidade da cláusula (using) acima
+
+      s.Empilha(1);
+      s.Empilha(10);
+      s.Empilha(100);
+      Console.WriteLine(s.Desempilha());
+      Console.WriteLine(s.Desempilha());
+      Console.WriteLine(s.Desempilha());
+      Console.WriteLine(s.Desempilha()); // estourando a seção do Desempilha(); pilha vazia
+    }
+  }
+}
